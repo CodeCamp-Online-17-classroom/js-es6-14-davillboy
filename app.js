788af72eba4f86bca7709a5aa5742515bbec58fe
davillboy <71083807+davillboy@users.noreply.github.com>
2024-04-24 17:49:07 +0700

    first change

--- app.js
+++ app.js
@@ -1,1 +1,5 @@
 // write code here
+let facts = { numPlanets: 8, yearNeptuneDiscovered: 1846 };
+let { numPlanets, yearNeptuneDiscovered } = facts;
+console.log(numPlanets); 
+console.log(yearNeptuneDiscovered); 
